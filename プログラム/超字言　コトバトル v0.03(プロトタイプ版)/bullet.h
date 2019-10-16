7150270d56f655c2508a11964567d30164c4f59a
kodama yuto <jb2017013@stu.yoshida-g.ac.jp>
2019-10-16 15:04:03 +0900

    v0.03 Bullet制作準備

---
+++ プログラム/超字言　コトバトル v0.03(プロトタイプ版)/bullet.h
@@ -0,0 +1,28 @@
+//
+//
+//
+//
+//
+//
+#ifndef _BULLET_H_
+#define _BULLET_H_
+
+#include "scene.h"
+class CSceneX;
+class CSceneBillBoard;
+
+class CBulletBase : public CScene
+{
+
+};
+
+class CModelBullet : public CBulletBase
+{
+
+};
+class CWordBullet : public CBulletBase
+{
+
+};
+
+#endif // !_BULLET_H_
