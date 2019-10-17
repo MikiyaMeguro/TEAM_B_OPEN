21bf8b81fb973bf2b2ae85f5107bf64726863ad3
kodama yuto <jb2017013@stu.yoshida-g.ac.jp>
2019-10-17 11:12:59 +0900

    v0.03 Bullet制作準備②
    BulletManager設定

--- プログラム/超字言　コトバトル v0.03(プロトタイプ版)/bullet.h
+++ プログラム/超字言　コトバトル v0.03(プロトタイプ版)/bullet.h
@@ -1,9 +1,11 @@
+//=============================================================================
 //
+// íeèàóù [bullet.h]
+// Author : Kodama Yuto
 //
+//	ÉNÉâÉXÇÃäTóv
 //
-//
-//
-//
+//=============================================================================
 #ifndef _BULLET_H_
 #define _BULLET_H_
 
@@ -11,18 +13,62 @@
 class CSceneX;
 class CSceneBillBoard;
 
-class CBulletBase : public CScene
+class CBulletBase
 {
+public:
+	CBulletBase();
+	~CBulletBase();
 
+	virtual void Set(D3DXVECTOR3 pos,D3DXVECTOR3 rot);
+
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	D3DXVECTOR3& GetPosition(void) { return m_pos;};
+	D3DXVECTOR3& GetRotation(void) { return m_rot;};
+	D3DXVECTOR3& GetMove(void) { return m_Move; };
+
+	void SetID(int nID) { m_nID = nID; };
+private:
+	D3DXVECTOR3 m_pos;				//à íu
+	D3DXVECTOR3 m_rot;				//äpìx
+	D3DXVECTOR3 m_Move;				//à⁄ìÆó 
+
+	int m_nID;
 };
 
 class CModelBullet : public CBulletBase
 {
+public:
+	CModelBullet();
+	~CModelBullet();
+
+	void Set(D3DXVECTOR3 pos, D3DXVECTOR3 rot);
+
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+private:
 
 };
 class CWordBullet : public CBulletBase
 {
+public:
+	CWordBullet();
+	~CWordBullet();
 
+	void Set(D3DXVECTOR3 pos, D3DXVECTOR3 rot);
+
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+private:
 };
 
 #endif // !_BULLET_H_
