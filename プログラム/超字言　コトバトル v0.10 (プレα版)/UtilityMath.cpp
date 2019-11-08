74b79e9e1a92f14e3f7412bf0d60e2747d8a374c
kodama yuto <jb2017013@stu.yoshida-g.ac.jp>
2019-11-08 11:48:33 +0900

    v0.10 CUtilityMathクラス改修

--- プログラム/超字言　コトバトル v0.10 (プレα版)/UtilityMath.cpp
+++ プログラム/超字言　コトバトル v0.10 (プレα版)/UtilityMath.cpp
@@ -1,6 +1,6 @@
 //=============================================================================
 //
-// 汎用数学クラス [UtilityMath.cpp]
+// 汎用数学クラス [UtilityMath.cpp](CUtilityMath)
 // Author : Kodama Yuto
 //
 //=============================================================================
@@ -8,13 +8,14 @@
 
 
 //=============================================================================
-//	マトリックス計算関数
+//	ワールドマトリックス計算関数
 //=============================================================================
 void CUtilityMath::CalWorldMatrix(D3DXMATRIX* pOut, const D3DXVECTOR3& pos, const D3DXVECTOR3& rot,
 	const D3DXMATRIX* parent,const D3DXVECTOR3& scale)
 {
 	D3DXMATRIX mtxRot, mtxTrans, mtxScale, mtxParent;				// 計算用マトリックス
-																	// ワールドマトリックスの初期化
+
+	// ワールドマトリックスの初期化
 	D3DXMatrixIdentity(pOut);
 
 	// 回転を反映
@@ -29,12 +30,12 @@ void CUtilityMath::CalWorldMatrix(D3DXMATRIX* pOut, const D3DXVECTOR3& pos, cons
 	D3DXMatrixTranslation(&mtxTrans, pos.x, pos.y, pos.z);
 	D3DXMatrixMultiply(pOut, pOut, &mtxTrans);
 
-
 	if (parent != NULL)
 	{
 		//親のマトリックスを掛け合わせる
 		D3DXMatrixMultiply(pOut,pOut,parent);
 	}
+
 }
 
 //=============================================================================
@@ -55,9 +56,16 @@ void CUtilityMath::RotateNormarizePI(float* value)
 //=============================================================================
 //	数値の線形写像関数
 //=============================================================================
-float CUtilityMath::Mapping(const float& value, const float& fromSource, const float& toSource, const float& fromTarget, const float& toTarget)
+float CUtilityMath::Mapping(const float& value, const float& fromSource, const float& toSource, const float& fromTarget, const float& toTarget,bool bClamp)
 {
-	return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+	float fResult = (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+
+	if (bClamp == true)
+	{
+		if (fResult > toTarget) { fResult = toTarget; }
+		else if (fResult < fromTarget) { fResult = fromTarget; }
+	}
+	return fResult;
 }
 
 //=============================================================================
@@ -73,7 +81,7 @@ D3DXVECTOR3 CUtilityMath::MoveCoeffient(D3DXVECTOR3& value, const float& coeffie
 }
 
 //=============================================================================
-//	任意の桁数でfloatを切り上げる関数
+//	任意の桁数でfloatを四捨五入する関数
 //=============================================================================
 float CUtilityMath::Round_n(float& fValue, const int nRound)
 {
@@ -82,4 +90,52 @@ float CUtilityMath::Round_n(float& fValue, const int nRound)
 	fValue /= pow(10.0f, (float)(nRound - 1));  //10の(n-1)乗で割る。
 
 	return fValue;
+}
+
+//=============================================================================
+//
+// ３次元ベクトルクラス [UtilityMath.cpp](VECTOR_3D)
+// Author : Kodama Yuto
+//
+//=============================================================================
+//=============================================================================
+//	角度を法線として使えるように線形写像する関数
+//=============================================================================
+VECTOR_3D VECTOR_3D::RotateNormalize(void)
+{
+	//角度を正規化
+	//X
+	if (this->X > _PI)
+	{
+		this->X -= _PI * 2.0f;
+	}
+	else if (this->X < -_PI)
+	{
+		this->X += _PI * 2.0f;
+	}
+	//Y
+	if (this->Y > _PI)
+	{
+		this->Y -= _PI * 2.0f;
+	}
+	else if (this->Y < -_PI)
+	{
+		this->Y += _PI * 2.0f;
+	}
+	//Z
+	if (this->Z > _PI)
+	{
+		this->Z -= _PI * 2.0f;
+	}
+	else if (this->Z < -_PI)
+	{
+		this->Z += _PI * 2.0f;
+	}
+
+	//-1.0fから1.0fまでの数値に直す
+	this->X = CUtilityMath::Mapping(this->X, -_PI, _PI, -1.0f, 1.0f, true);
+	this->Y = CUtilityMath::Mapping(this->Y, -_PI, _PI, -1.0f, 1.0f, true);
+	this->Z = CUtilityMath::Mapping(this->Z, -_PI, _PI, -1.0f, 1.0f, true);
+
+	return *this;
 }
--- プログラム/超字言　コトバトル v0.10 (プレα版)/UtilityMath.h
+++ プログラム/超字言　コトバトル v0.10 (プレα版)/UtilityMath.h
@@ -13,16 +13,152 @@
 
 #include "main.h"
 
+#define _PI (3.141592f)
 //=============================================================================
-//	クラス定義
+//	クラス定義(VECTOR_3D)	独自の３次元ベクトルクラス
+//=============================================================================
+class VECTOR_3D
+{
+public:
+	float X;	//X軸の値
+	float Y;	//Y軸の値
+	float Z;	//Z軸の値
+
+	//コンストラクタ(引数なし)
+	VECTOR_3D()
+	{
+		this->X = 0.0f;
+		this->Y = 0.0f;
+		this->Z = 0.0f;
+	}
+	//コンストラクタ(引数あり)
+	VECTOR_3D(const float& X, const float& Y, const float& Z)
+	{
+		this->X = X;
+		this->Y = Y;
+		this->Z = Z;
+	}
+	//デストラクタ(デフォルト)
+	~VECTOR_3D() {};
+
+	//独自関数
+	VECTOR_3D RotateNormalize(void);
+
+	/*オペレータ*/
+	//代入
+	void operator = (const VECTOR_3D& vec)
+	{
+		this->X = vec.X;
+		this->Y = vec.Y;
+		this->Z = vec.Z;
+	}
+
+	//足し算
+	VECTOR_3D operator + (const VECTOR_3D& vec)
+	{
+		this->X = this->X + vec.X;
+		this->Y = this->Y + vec.Y;
+		this->Z = this->Z + vec.Z;
+
+		return *this;
+	}
+	void operator += (const VECTOR_3D& vec)
+	{
+		this->X += vec.X;
+		this->Y += vec.Y;
+		this->Z += vec.Z;
+	}
+
+	//引き算
+	VECTOR_3D operator - (const VECTOR_3D& vec)
+	{
+		this->X = this->X - vec.X;
+		this->Y = this->Y - vec.Y;
+		this->Z = this->Z - vec.Z;
+
+		return *this;
+	}
+	void operator -= (const VECTOR_3D& vec)
+	{
+		this->X -= vec.X;
+		this->Y -= vec.Y;
+		this->Z -= vec.Z;
+	}
+
+	//掛け算
+	VECTOR_3D operator * (const VECTOR_3D& vec)
+	{
+		this->X = this->X * vec.X;
+		this->Y = this->Y * vec.Y;
+		this->Z = this->Z * vec.Z;
+
+		return *this;
+	}
+	void operator *= (const VECTOR_3D& vec)
+	{
+		this->X *= vec.X;
+		this->Y *= vec.Y;
+		this->Z *= vec.Z;
+	}
+
+	//割り算
+	VECTOR_3D operator / (const VECTOR_3D& vec)
+	{
+		this->X = this->X / vec.X;
+		this->Y = this->Y / vec.Y;
+		this->Z = this->Z / vec.Z;
+
+		return *this;
+	}
+	void operator /= (const VECTOR_3D& vec)
+	{
+		this->X /= vec.X;
+		this->Y /= vec.Y;
+		this->Z /= vec.Z;
+	}
+
+	//定数掛け算
+	VECTOR_3D operator * (const float& fVal)
+	{
+		this->X = this->X * fVal;
+		this->Y = this->Y * fVal;
+		this->Z = this->Z * fVal;
+
+		return *this;
+	}
+
+	//比較(可読性のために三項演算子を使う)
+	bool operator == (const VECTOR_3D& vec)
+	{// ==
+		return (this->X == vec.X && this->Y == vec.Y && this->Z == vec.Z) ? true : false;
+	}
+	bool operator > (const VECTOR_3D& vec)
+	{// >
+		return (this->X > vec.X && this->Y > vec.Y && this->Z > vec.Z) ? true : false;
+	}
+	bool operator >= (const VECTOR_3D& vec)
+	{// >=
+		return (this->X >= vec.X && this->Y >= vec.Y && this->Z >= vec.Z) ? true : false;
+	}
+	bool operator < (const VECTOR_3D& vec)
+	{// <
+		return (this->X < vec.X && this->Y < vec.Y && this->Z < vec.Z) ? true : false;
+	}
+	bool operator <= (const VECTOR_3D& vec)
+	{// <=
+		return (this->X <= vec.X && this->Y <= vec.Y && this->Z <= vec.Z) ? true : false;
+	}
+};
+
+//=============================================================================
+//	クラス定義(CUtilityMath)
 //=============================================================================
 class CUtilityMath
 {
 public:
-
 	static void CalWorldMatrix(D3DXMATRIX* pOut,const D3DXVECTOR3& pos, const D3DXVECTOR3& rot, const D3DXMATRIX* parent = NULL, const D3DXVECTOR3& scale = D3DXVECTOR3(1.0f,1.0f,1.0f));
 	static void RotateNormarizePI(float* value);
-	static float Mapping(const float& value, const float& fromSource, const float& toSource, const float& fromTarget, const float& toTarget);
+	static float Mapping(const float& value, const float& fromSource, const float& toSource, const float& fromTarget, const float& toTarget, bool bClamp = false);
 	static D3DXVECTOR3 MoveCoeffient(D3DXVECTOR3& value,const float& coeffient);
 	static float Round_n(float& fValue,const int nRound);
 };
