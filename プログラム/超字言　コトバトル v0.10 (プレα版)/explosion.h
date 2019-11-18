65576b895c89cddc7dcaef8be0f8b6191514b06b
kodama yuto <jb2017013@stu.yoshida-g.ac.jp>
2019-11-18 14:11:04 +0900

    v0.10　爆発(のようなもの)追加
    これから装飾するからそっとしといて

---
+++ プログラム/超字言　コトバトル v0.10 (プレα版)/explosion.h
@@ -0,0 +1,37 @@
+//=============================================================================
+//
+// 爆発クラス [explosion.h]
+// Author : Kodama Yuto
+//
+//	クラスの概要:
+//
+//=============================================================================
+#ifndef _EXPLOSION_H_
+#define _EXPLOSION_H_
+
+#include "meshSphere.h"
+
+class CExplosion3D : public CMeshSphere
+{
+public:
+	CExplosion3D();
+	CExplosion3D(int pri = 1, OBJTYPE type = OBJTYPE_SCENE3D);
+	~CExplosion3D();
+
+	static CExplosion3D* Create(void);
+
+	void Set(D3DXVECTOR3 pos,float fSize,int nLife);
+
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	bool Collision(const D3DXVECTOR3& pos, const D3DXVECTOR3& posOld);
+private:
+	float m_fSize;
+	float m_fOrgSize;
+	int m_nLife;
+};
+
+#endif // !_EXPLOSION_H_
