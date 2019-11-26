a466a1b7ecf5c432f7214aad84e3b1e0e79e12c8
横道 <jb2017035@stu.yoshida-g.ac.jp>
2019-11-26 12:13:40 +1000

    修正

--- プログラム/超字言　コトバトル v0.10 (プレα版)/SetWord.cpp
+++ プログラム/超字言　コトバトル v0.10 (プレα版)/SetWord.cpp
@@ -163,7 +163,10 @@ void CSetWord::WordUninit(void)
 			if (pWord->GetUninitFlag() == true)
 			{
 				nNum = pWord->GetNum();		// ”Ô†‚ğæ“¾
-				m_pWordPos[nNum].bUse = false;
+				if (nNum < m_nNum)
+				{	// “y‘ä‚©‚ço‚Ä‚«‚½•¶š‚È‚ç
+					m_pWordPos[nNum].bUse = false;
+				}
 				pWord->Uninit();
 				pWord = NULL;
 			}
