761fea2db07622796f136dde5848c708c1583120
横道 <jb2017035@stu.yoshida-g.ac.jp>
2019-11-28 16:32:30 +1000

    ポイント変更

--- プログラム/超字言　コトバトル v0.10 (プレα版)/player.cpp
+++ プログラム/超字言　コトバトル v0.10 (プレα版)/player.cpp
@@ -712,10 +712,17 @@ bool CPlayer::CollisionDamageObj(void)
 						if (CManager::GetMode() == CManager::MODE_GAME) { pPoint = CGame::GetPoint(pBullet->GetID()); }
 						if (CManager::GetMode() == CManager::MODE_TUTORIAL) { pPoint = CTutorial::GetPoint(pBullet->GetID()); }
 
-						if (pPoint != NULL) { pPoint->AddPoint(1); }
+						CModelBullet *pModelBullet = ((CModelBullet*)pBullet);
+
+						int nPoint = 0;
+						if (pModelBullet->GetType() == CModelBullet::TYPE_NORMAL) { nPoint = 1; }
+						else if (pModelBullet->GetType() != CModelBullet::TYPE_NORMAL) { nPoint = 3; }
+
+						if (pPoint != NULL) { pPoint->AddPoint(nPoint); }
 						pSound->SetVolume(CSound::SOUND_LABEL_SE_POINTUP, 3.0f);
 						pSound->PlaySound(CSound::SOUND_LABEL_SE_POINTUP);
 
+						C3DBullet *pBullet = ((C3DBullet*)pModelBullet);		// CBulletBaseへキャスト(型の変更)
 					}
 
 					//吹き飛ばし
