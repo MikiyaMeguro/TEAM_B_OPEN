88ddf9dee3018eb06a5a037dc99f7fe0138522c3
kodama yuto <jb2017013@stu.yoshida-g.ac.jp>
2019-12-12 14:16:25 +0900

    v0.8 変更箇所を最新版に反映

--- プログラム/超字言　コトバトル v0.8 (β版)/pause.cpp
+++ プログラム/超字言　コトバトル v0.8 (β版)/pause.cpp
@@ -16,8 +16,12 @@ CPause::CPause(int nPriority) : CScene(nPriority,OBJTYPE_PAUSE)
 {
 	m_pPause = NULL;
 	m_pBG = NULL;
-	for (int nCnt = 0; nCnt < 4; nCnt++) { m_apSelectMenu[nCnt] = NULL; }
+	for (int nCnt = 0; nCnt < 4; nCnt++) {
+		m_apSelectMenu[nCnt] = NULL;
+		m_aMenuDefaultPos[nCnt] = D3DXVECTOR3(0.0f,0.0f,0.0f);
+	}
 	m_nSelect = 0;
+	m_nCount = 0;
 }
 CPause::~CPause()
 {
@@ -53,24 +57,25 @@ HRESULT CPause::Init(void)
 	{
 		m_pBG->SetObjType(OBJTYPE_PAUSE);
 		m_pBG->SetWidthHeight(SCREEN_WIDTH,SCREEN_HEIGHT);
-		m_pBG->SetCol(D3DXCOLOR(0.5f,0.5f,0.5f,0.75f));
+		m_pBG->SetCol(D3DXCOLOR(0.5f,0.5f,0.5f,0.65f));
 	}
 
 	//ポーズ文字の生成
-	m_pPause = CScene2D::Create(D3DXVECTOR3(SCREEN_WIDTH / 2.0f,50.0f,0.0f),"",7);
+	m_pPause = CScene2D::Create(D3DXVECTOR3(SCREEN_WIDTH / 2.0f,75.0f,0.0f),"",7);
 	if (m_pPause != NULL)
 	{
 		m_pPause->SetObjType(OBJTYPE_PAUSE);
-		m_pPause->SetWidthHeight(100.0f,50.0f);
+		m_pPause->SetWidthHeight(400.0f,80.0f);
 	}
 	//セレクトの生成
 	for (int nCnt = 0; nCnt < 4; nCnt++)
 	{
-		m_apSelectMenu[nCnt] = CScene2D::Create(D3DXVECTOR3(SCREEN_WIDTH / 2.0f, 100.0f + (nCnt * 50.0f), 0.0f), "",7);
+		m_aMenuDefaultPos[nCnt] = D3DXVECTOR3(SCREEN_WIDTH / 2.0f, 210.0f + (nCnt * 150.0f), 0.0f);
+		m_apSelectMenu[nCnt] = CScene2D::Create(m_aMenuDefaultPos[nCnt], "",7);
 		if (m_apSelectMenu[nCnt] != NULL)
 		{
 			m_apSelectMenu[nCnt]->SetObjType(OBJTYPE_PAUSE);
-			m_apSelectMenu[nCnt]->SetWidthHeight(200.0f, 100.0f);
+			m_apSelectMenu[nCnt]->SetWidthHeight(200.0f, 60.0f);
 
 		}
 
@@ -111,6 +116,28 @@ void CPause::Uninit(void)
 //=============================================================================
 void CPause::Update(void)
 {
+	m_nCount++;
+	float fCntHover = (float)(m_nCount +1);
+
+	for (int nCnt = 0; nCnt < 4; nCnt++)
+	{
+		if (m_apSelectMenu[nCnt] != NULL)
+		{
+			m_apSelectMenu[nCnt]->SetWidthHeight(200.0f, 60.0f);
+		}
+	}
+
+	if (CCommand::GetCommand("UP"))
+	{
+		m_nSelect = (m_nSelect + 3) % 4;
+	}
+	else if (CCommand::GetCommand("DOWN"))
+	{
+		m_nSelect = (m_nSelect + 1) % 4;
+	}
+
+	m_apSelectMenu[m_nSelect]->SetPosition(m_aMenuDefaultPos[m_nSelect] + D3DXVECTOR3(0.0f, cosf(fCntHover / 5.0f) * 1.5f, 0.0f));
+	m_apSelectMenu[m_nSelect]->SetWidthHeight(300.0f, 90.0f);
 
 }
 
